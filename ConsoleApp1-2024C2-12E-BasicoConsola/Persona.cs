39112129d52f4eb01074d631c5c68f77100283dd
christian.slapak <christian.slapak@ort.edu.ar>
2024-09-18 20:25:33 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1-2024C2-12E-BasicoConsola/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_2024C2_12E_BasicoConsola
+{
+    internal class Persona
+    {
+        public int DNI { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+
+        public string MostrarDatos()
+        {
+            string mensaje;
+            mensaje = "Los datos son: " + DNI + " " + Apellido + " " + Nombre;
+            return mensaje;
+        }
+    }
+}
---
+++ ConsoleApp1-2024C2-12E-BasicoConsola/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1_2024C2_12E_BasicoConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Persona p = new Persona();
+
+            p.DNI = 12456889;
+            p.Apellido = "Martinez";
+            p.Nombre = "Daniel";
+
+            Console.WriteLine(p.MostrarDatos());
+
+            Console.ReadKey();
+        }
+    }
+}
